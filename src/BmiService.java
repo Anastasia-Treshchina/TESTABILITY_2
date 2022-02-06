d88bc901f08517ab0ad371a94d567be77519cfc8
Anastasia Treshchina <anastasiya.treschina@gmail.com>
2022-02-06 19:01:17 +0400

    formula change

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,6 @@
 public class BmiService {
     public float calculate(float weight, float height) {
-        float a = 2;
-        float bmi = weight * 10_000/ ((float) Math.pow (height, a));
+        float bmi = weight * 10_000/ ((float) Math.pow (height, 2));
         return bmi;
     }
 }
