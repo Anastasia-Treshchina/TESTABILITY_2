17fe113a5e6f5daad9269b682c0526bf7dd7fd1c
Anastasia Treshchina <anastasiya.treschina@gmail.com>
2022-02-06 19:00:07 +0400

    formula change

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,7 @@
 public class BmiService {
     public float calculate(float weight, float height) {
-        float bmi = weight * 10_000/ (height * height);
+        float a = 2;
+        float bmi = weight * 10_000/ ((float) Math.pow (height, a));
         return bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        float bmi = service.calculate(75, 169);
+        float bmi = service.calculate(74, 172);
         String result = String.format("%.2f", bmi);
         System.out.println("Индекс массы тела составляет: " + result + " кг/м2");
     }
